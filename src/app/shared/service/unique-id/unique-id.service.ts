fba7d22c83fd47e60fce88b23a4c622080937a25
Paloma <palomacorreiadasilva@hotmail.com>
2021-09-12 17:32:27 -0300

    Adicionando teste na condição de Id Válido

--- src/app/shared/service/unique-id/unique-id.service.ts
+++ src/app/shared/service/unique-id/unique-id.service.ts
@@ -4,9 +4,10 @@ import {v4 as uuidv4} from 'uuid';
 @Injectable()
 export class UniqueIdService {
     private numberOfGeneratedIds = 0;
+    private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
 
     public generatedUniqueIdwithPrefix(prefix : string): string {
-        if(!prefix){
+        if(!prefix || !this.validId.test(prefix)){
             throw Error('PREFIX can not be empty');
         }
         const uniqueId = this.generatedUniqueId();

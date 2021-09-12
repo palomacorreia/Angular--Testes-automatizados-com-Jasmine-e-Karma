7a578b95b5878b844d15338854f28bc2a05fbe6e
Paloma <palomacorreiadasilva@hotmail.com>
2021-09-12 17:14:54 -0300

    teste 4- prefix not null(Exception)

--- src/app/shared/service/unique-id/unique-id.service.spec.ts
+++ src/app/shared/service/unique-id/unique-id.service.spec.ts
@@ -12,24 +12,34 @@
 import { UniqueIdService } from "./unique-id.service";
 
 describe(UniqueIdService.name, () => {
+    let service = null;
+
+    beforeEach(() => {
+        service = new UniqueIdService();
+    });
+
     it(`#${UniqueIdService.prototype.generatedUniqueIdwithPrefix.name} should generate id when called prefix`, () => {
-        const service = new UniqueIdService();
         const id = service.generatedUniqueIdwithPrefix('app');
         expect(id.startsWith('app-')).toBeTrue();
         
     });
     
     it(`#${UniqueIdService.prototype.generatedUniqueIdwithPrefix.name} should not generate duplicate ids when called`, () => {
-        const service = new UniqueIdService();
         const ids = new Set();
         for(let i =0; i<50; i++){
             ids.add(service.generatedUniqueIdwithPrefix('app'));
         }
         expect(ids.size).toBe(50);
+    });    
+    
+    it(`#${UniqueIdService.prototype.generatedUniqueIdwithPrefix.name} should throw when called with empty`, () => {
+        const emptyValues = [null,undefined, ''];
+        emptyValues.forEach(emptyValues =>{
+            expect(() => service.generatedUniqueIdwithPrefix(emptyValues)).toThrow();
+        })
     });   
     
     it(`#${UniqueIdService.prototype.getNumbergeneratedUniqueIds.name} should return the number of generatedIds when called`, () => {
-        const service = new UniqueIdService();
         service.generatedUniqueIdwithPrefix('app');
         service.generatedUniqueIdwithPrefix('app');
         expect(service.getNumbergeneratedUniqueIds()).toBe(2);
